f13f3cc3786bec917aecb8909b4cbd522ab11b4e
StoyanTrojan <stoyan_iliev@abv.bg>
2019-09-13 20:46:54 +0300

    Added a left and right track throttle.

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -29,9 +29,6 @@ void AProjectile::Tick(float DeltaTime)
 
 void AProjectile::LaunchProjectile(float Speed) {
 
-	auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("%f. Tank is firing.%f"), Time,Speed);
-
 	ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector*Speed);
 	ProjectileMovement->Activate();
 }
